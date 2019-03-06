c53040651787d727cb05ffd4be4a56b26b50d9e0
Rohit599 <rjakhmola5@gmail.com>
2019-03-06 22:58:01 +0530

    add rules

--- rule.php
+++ rule.php
@@ -1,25 +1,26 @@
 <?php
-include 'config.php';
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+session_start();
+// include'config.php';
 include 'conf.php';
 ?>
 <html>
 <head>
-    <title>Rules | Night Knitting</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <?php include 'head.php'; ?>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <div><img src="assets/images/logo-full.png" margin="auto"></div>
-    <h1>BE WARNED</h1>
-    <div class="table-container">
-    <h4 style="margin-bottom: 2rem">Rules.</h4>
-    <h4>1. use all means necessary to progress to the next level</h4>
-    <h4>2. look for hints everywhere you think they can be found</h4>
-    <h4>3. the game can be played only from 8pm to 8am</h4>
-    <h4>4. answer should be in lowercase without spaces</h4>
-    <h4>5. reject only your ignorance and you may survive</h4>
-</div>
-<div style="padding-top: 6rem; padding-left: 8rem; text-align: left;"><a href="home.php" style="font-size: 1.5rem; font-weight: bold; text-decoration: underline; position: fixed; bottom: 30;">Home.</a>
+    <img src="assets/images/logo-full.png" style="height: 15rem;">
+
+    <div class="sign">
+        <p>Rules.</p>
+    <p>1. use all means necessary to progress to the next level</p>
+    <p>2. look for hints everywhere you think they can be found</p>
+    <p>3. the game can be played only from 10pm to 10am</p>
+    <p>4. answer should be in lowercase without spaces</p>
+    <p>5. reject only your ignorance and you may survive</p>
     </div>
+
 </body>
 </html>
